22f21203006d1d7ee73809e7f422b60a43c7bccb
Adrivid <adrividmishra@gmail.com>
2026-02-27 20:52:12 +0530

    BS -> BS on Answers -> 13

---
+++ Module 4 Binary Search/BS on Answers/13_median_of_two_sorted_arrays.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+double findMedianSortedArraysBrute(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> arr;
+    // arr.insert(arr.end(),nums1.begin(),nums1.end());
+    // arr.insert(arr.end(),nums2.begin(),nums2.end());
+    // sort(arr.begin(),arr.end());
+    int i=0,j=0;
+    int n1=nums1.size(),n2=nums2.size();
+    while(i<n1 && j<n2){
+        if(nums1[i]<nums2[j]){
+            arr.push_back(nums1[i++]);
+        }else{
+            arr.push_back(nums2[j++]);
+        }
+    }
+    while(i<n1){
+        arr.push_back(nums1[i++]);
+    }
+    while(j<n2){
+        arr.push_back(nums2[j++]);
+    }
+
+    int n=n1+n2;
+    if(n%2==0){
+        return (arr[n/2]+arr[n/2-1])/2.0;
+    }else{
+        return arr[n/2];
+    }
+}
+
+double findMedianSortedArraysBetter(vector<int>& nums1, vector<int>& nums2) {
+    int i=0,j=0;
+    int n1=nums1.size(),n2=nums2.size();
+    int n=n1+n2;
+    int count=0;
+    int index1=n/2-1,index2=n/2;
+    int index1Element=-1,index2Element=-1;
+    while(i<n1 && j<n2){
+        if(nums1[i]<nums2[j]){
+            if(count==index1) index1Element=nums1[i];
+            if(count==index2) index2Element=nums1[i];
+            count++;
+            i++;
+        }else{
+            if(count==index1) index1Element=nums2[j];
+            if(count==index2) index2Element=nums2[j];
+            count++;
+            j++;    
+        }
+    }
+    while(i<n1){
+        if(count==index1) index1Element=nums1[i];
+        if(count==index2) index2Element=nums1[i];
+        count++;
+        i++;
+    }
+    while(j<n2){
+        if(count==index1) index1Element=nums2[j];
+        if(count==index2) index2Element=nums2[j];
+        count++;
+        j++;     
+    }
+
+    if(n%2==0){
+        return (index1Element+index2Element)/2.0;
+    }else{
+        return index2Element;
+    }
+}
+
+double findMedianSortedArraysOptimal(vector<int>& nums1, vector<int>& nums2) {
+    int n1=nums1.size();
+    int n2=nums2.size();
+    int n=n1+n2;
+    if(n1>n2) return findMedianSortedArraysOptimal(nums2,nums1);   //RUN BS only on the smaller array
+    int partition=(n1+n2+1)/2;
+    int low=0,high=n1;
+    while(low<=high){
+        int mid1=(low+high)/2;
+        int mid2=partition-mid1;
+        int l1=INT_MIN,l2=INT_MIN;
+        int r1=INT_MAX,r2=INT_MAX;
+        if(mid1<n1) r1=nums1[mid1];
+        if(mid2<n2) r2=nums2[mid2];
+        if(mid1-1>=0) l1=nums1[mid1-1];
+        if(mid2-1>=0) l2=nums2[mid2-1];
+
+        if(l1<=r2 && l2<=r1){   //found answer
+            if(n%2==0){
+                return (max(l1,l2)+min(r1,r2))/2.0;
+            }else{
+                return max(l1,l2);
+            }
+        }
+        else if(l1>r2) high=mid1-1;
+        else low=mid1+1;   // l2>r1
+    }
+    return -1;
+}
+int main(){
+    vector<int> nums1={1,2,5,8,13,45};
+    vector<int> nums2={3,4,56,455,788};
+    
+    cout<<findMedianSortedArraysBrute(nums1,nums2)<<endl;
+    cout<<findMedianSortedArraysBetter(nums1,nums2)<<endl;
+    cout<<findMedianSortedArraysOptimal(nums1,nums2)<<endl;
+
+    return 0;
+}
