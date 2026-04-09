e01e606288747da980003d951780c6c0a69bd93a
Adrivid <adrividmishra@gmail.com>
2026-04-10 00:00:24 +0530

    fixed 7

--- Module 6 Linked List/LL Medium/7_add_one_to_a_number_represented_by_LL.cpp
+++ Module 6 Linked List/LL Medium/7_add_one_to_a_number_represented_by_LL.cpp
@@ -1,4 +1,4 @@
-//LC 234
+//NO LC 
 #include<iostream>
 #include<vector>
 
@@ -15,24 +15,41 @@ class ListNode {
 
 class Solution {
 public:
-    ListNode *addOne(ListNode* head) {
-        ListNode *newHead=reverseList(head);
-        ListNode *storeNewHead=newHead;
+    ListNode *addOneIterative(ListNode* head) {
+        // ListNode *newHead=reverseList(head);
+        // ListNode *storeNewHead=newHead;
         
-        while(newHead!=nullptr){
-            if(newHead->val==9){
-                newHead->val=0;
-                if(newHead->next==nullptr){
-                    newHead->next=new ListNode(1);
-                    break;
-                }
-            }else{
-                newHead->val=newHead->val+1;
-                break;
-            }
-            newHead=newHead->next;
+        // while(newHead!=nullptr){
+        //     if(newHead->val==9){
+        //         newHead->val=0;
+        //         if(newHead->next==nullptr){
+        //             newHead->next=new ListNode(1);
+        //             break;
+        //         }
+        //     }else{
+        //         newHead->val=newHead->val+1;
+        //         break;
+        //     }
+        //     newHead=newHead->next;
+        // }
+        // return reverseList(storeNewHead);
+        head=reverseList(head);
+        int carry=1;    //adding one to LL
+
+        ListNode *temp=head;
+        while(temp!=nullptr){
+            int newVal=carry+temp->val;
+            carry=newVal/10;
+            temp->val=newVal%10;
+            if(carry==0) break;
+
+            temp=temp->next;
         }
-        return reverseList(storeNewHead);
+        head=reverseList(head);
+        if(carry){
+            head=new ListNode(carry,head);
+        }
+        return head;
     }
 
     ListNode* reverseList(ListNode* head) {
@@ -47,6 +64,29 @@ public:
         }
         return prev;
     }
+
+    ListNode *addOneRecursive(ListNode *head){
+        int carry=helper(head);
+
+        if(carry){
+            ListNode *newHead=new ListNode(1,head);
+            head=newHead;
+        }
+        return head;
+    }
+
+    int helper(ListNode *temp){
+        if(temp==nullptr){
+            return 1;
+        }
+
+        int carry=helper(temp->next);
+        temp->val+=carry;
+        if(temp->val<10) return 0;
+        temp->val=0;
+        return 1;
+    }
+
 };
 
 // Utility functions
@@ -79,7 +119,11 @@ int main(){
     cout<<endl;
 
     Solution obj;
-    ll=obj.addOne(ll);
+    ll=obj.addOneIterative(ll);
+    printLL(ll);
+    cout<<endl;
+
+    ll=obj.addOneRecursive(ll);
     printLL(ll);
 
     return 0;
